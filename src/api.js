6b175613f3227d61db117551fd3b6e5b562ded66
superastorh <salem.m.s2030@gmail.com>
2021-02-15 10:00:08 +0300

    fast edit

--- src/api.js
+++ src/api.js
@@ -2,59 +2,29 @@ const express = require("express");
 
 const app = express();
 
-const Ranidb = require('../ranidb/index');
+const Ranidb = require('ranidb');
 
-let db = new Ranidb("./data/data.json", {idType: 'gradual'});
+let db = new Ranidb("./data/data.json");
 
 app.use(express.json());
-
 
 app.get('/', (req, res) => {
     res.send(`<a href="./api/users"> user </a>`)
 })
+
 
 app.get('/api/users', (req, res) => {
     res.send(db.getAll())
 })
 
 app.get('/api/users/:id', (req, res) => {
-    res.send(db.isId(req.params.id) || '<h1>not find this id</h1>');
-})
-/*
-app.post('/api/users/', (req, res) => {
-
-    db.push(
-        {
-            telegram: req.body.telegram,
-            username: req.body.username
-        }
-    )
-    res.send(db.getAll())
+    res.send(db.find({_id : req.params.id}));
 })
 
-app.delete('/api/users/:id', (req, res) => {
-    const id = req.params.id;
-    if (db.isId(id) !== undefined) {
-
-        db.filter({_id: parseInt(id)}).delete();
-
-        res.status(200).send(db.getAll());
-
-    } else {
-        res.status(404).send("not find it")
-    }
-})
-*/
 const port = process.env.PORT || 5000;
 
 app.listen(
     port, () => {
-        console.log("run on port " + port)
+        console.log("run on port \n" + "http://localhost:" + port)
     }
 )
-
-/*
-db.isId = function (id) {
-    return db.find({_id: parseInt(id)}) || undefined;
-}
-*/
